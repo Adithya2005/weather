9a01c63ca28e5ffe027b1b4ce78c5111990edfba
Adithya <90098512+Adithya2005@users.noreply.github.com>
2024-08-28 18:28:35 +0530

    modified:   server.js

--- server.js
+++ server.js
@@ -119,7 +119,7 @@ app.post('/', async (req, res) => {
 });
 
 //To get Specific date in mongoDb
-app.get('/weather', async (req, res) => {
+app.get('/', async (req, res) => {
   try {
     let city = req.query.city + ',UK';
     let startdate = req?.query?.startdate ?? new Date().toJSON().slice(0, 10);
@@ -155,11 +155,6 @@ app.get('/weather', async (req, res) => {
   })
 }
 });
-app.get('/',(req,res)=>{
-  res.json({
-    message : "Weather API is working fine",
-  })
-})
 
 app.delete('/', async (req, res) => {
   let weatherid = req.query.id;
